626b44928140c1270b71b97a7a77eb6163d838c3
hsuhau <hsuhau@foxmail.com>
2021-10-30 12:35:41 +0800

    Initial commit

---
+++ src/hello.groovy
@@ -0,0 +1,12 @@
+//class hello {
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Hello World");
+//        }
+
+//        for( i in 0..2){ println "Hello World" }
+
+        0.upto(2) { println "Hello World" }
+//    }
+
+//}
